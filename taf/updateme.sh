0a98c734d2a084b7ab8c8d582e30c823abbe06b9
Vishwas SP <vishwasx.s.premachandra@intel.com>
2019-07-16 13:15:26 -0700

    Updated tools list
    Signed-off-by: Vishwas SP <vishwasx.s.premachandra@intel.com>

--- taf/updateme.sh
+++ taf/updateme.sh
@@ -2,7 +2,7 @@
 #This file is used to update your local OS environment to get project
 #dependencies.
 sudo apt -y update
-sudo apt -y install python3.6
+#sudo apt -y install python3.6
 sudo apt -y install python3-pip
 sudo apt -y install git
 pip3 install -r requirements.txt
